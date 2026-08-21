9b723e68d4d687601d6fce08fa7439ea8b61a722
soniceman1008-glitch <soniceman1008@gmail.com>
2026-08-21 16:18:03 +0300

    Show 03139235654 on the footer call link

--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -3,7 +3,7 @@ import { Phone } from "lucide-react";
 import { CallLink } from "@/components/call-link";
 import { useRestaurant } from "@/lib/catalog-store";
 
-const CALL_DISPLAY = "+923139235645";
+const CALL_DISPLAY = "03139235654";
 
 export function SiteFooter() {
   const shop = useRestaurant();
